597fa4b6573ae08afab446445f84af95333baaf6
rinsuki <428rinsuki+git@gmail.com>
2021-12-16 08:15:15 +0900

    use getpid as default value

--- Sources/csopsInSwift/csopsInSwift.swift
+++ Sources/csopsInSwift/csopsInSwift.swift
@@ -1,6 +1,6 @@
 import Darwin
 
-public func csops(pid: pid_t, ops: CUnsignedInt, dest: UnsafeMutableRawPointer?, destSize: size_t) -> CInt {
+public func csops(pid: pid_t = getpid(), ops: CUnsignedInt, dest: UnsafeMutableRawPointer?, destSize: size_t) -> CInt {
     let csopsRaw = dlsym(.init(bitPattern: -2), "csops")
     typealias csopsType = @convention(c) (pid_t, CUnsignedInt, UnsafeMutableRawPointer?, size_t) -> CInt
     return unsafeBitCast(csopsRaw, to: csopsType.self)(pid, ops, dest, destSize)
